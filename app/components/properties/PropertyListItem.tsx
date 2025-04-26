f2f84bf84d700b99b4f069108d49bf972380d637
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-04-26 14:15:49 +0530

    13 - Created docker container for backend , and added properties app

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import { PropertyType } from "./PropertyList";
 
-const PropertyListItem = () => {
+interface PropertyProps {
+    property : PropertyType
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property
+}) => {
     return (
     <div className="cursor-pointer">
         <div className="relative overflow-hidden aspect-square rounded-xl w-full h-[200px]">
         <Image
             fill
-            src="/beach_1.jpg"
+            src={property.image_url}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             className="hover:scale-110 object-cover transition"
             alt="Beach House"
@@ -14,11 +21,11 @@ const PropertyListItem = () => {
         </div>
 
         <div className="mt-2">
-            <p className="text-lg font-bold">Property Name</p>
+            <p className="text-lg font-bold">{property.title}</p>
         </div>
 
         <div className="mt-2">
-            <p className="text-sm text-gray-700 font-bold"><strong>$200</strong></p>
+            <p className="text-sm text-gray-700 font-bold"><strong>${property.price_per_night} per night</strong></p>
         </div>
     </div>
     );
