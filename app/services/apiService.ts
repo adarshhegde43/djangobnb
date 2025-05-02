533b7ac1087ec04c6b8ab40577cb51351bd7984a
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-05-02 16:15:56 +0530

    36 - stringified body data in postWithoutToken method

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -58,7 +58,7 @@ const apiService = {
         return new Promise((resolve, reject) => {
             fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
                 method: 'POST',
-                body: data,
+                body: JSON.stringify(data),
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
